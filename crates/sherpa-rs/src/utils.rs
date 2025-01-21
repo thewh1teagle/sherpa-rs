2717e6ff50e12dcb10c2b9e65cc77b8a496b18aa
Vitali Lovich <vlovich@openinfer.io>
2025-01-21 10:51:53 -0800

    Mark cstr_to_string as unsafe
    In Rust, having an unsafe interior conventionally means that you are
    guaranteeing safety at the boundary. However, that's not the case for
    cstr_to_string which is taking a random pointer as input. Thankfully,
    this doesn't functionally change anything anyway since it was already
    always called from an unsafe context.

--- crates/sherpa-rs/src/utils.rs
+++ crates/sherpa-rs/src/utils.rs
@@ -4,12 +4,10 @@ pub fn cstring_from_str(s: &str) -> CString {
     return CString::new(s).expect("CString::new failed");
 }
 
-pub fn cstr_to_string(ptr: *const c_char) -> String {
-    unsafe {
-        if ptr.is_null() {
-            String::new()
-        } else {
-            std::ffi::CStr::from_ptr(ptr).to_string_lossy().into_owned()
-        }
+pub unsafe fn cstr_to_string(ptr: *const c_char) -> String {
+    if ptr.is_null() {
+        String::new()
+    } else {
+        std::ffi::CStr::from_ptr(ptr).to_string_lossy().into_owned()
     }
 }
